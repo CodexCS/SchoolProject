526be9d18e8b00ffdbee260d1f85de3bc35b88fd
Arnav Bhagwat <76836227+Thindino@users.noreply.github.com>
2023-11-14 21:46:50 +0530

    Add files via upload

---
+++ primenum.java
@@ -0,0 +1,35 @@
+import java.util.*;
+/**
+ * Write a description of class primenum here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class primenum
+{
+    public static void main(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int n  = sc.nextInt();
+        int i = 0;
+        int m = 0;
+        int sum = 0;
+        for(i=1;i<=n;i++)
+            {
+            if(n%i==0)
+            {
+              //System.out.println(i + " ");  
+              sum = sum + i;
+            }
+        
+        }
+        if(sum==(n+1)){
+            System.out.println("This is prime number");
+        }
+        else
+        {
+            System.out.println("This is compisite number");
+        }
+        
+    } 
+}
