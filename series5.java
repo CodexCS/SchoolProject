9b26fde09ef1d774b25acbb14d5d293e57665cdf
Arnav Bhagwat <76836227+CodexCS@users.noreply.github.com>
2023-12-03 23:45:35 +0530

    1,11,111,1111...

---
+++ series5.java
@@ -0,0 +1,19 @@
+
+/**
+ * Write a description of class series5 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class series5
+{
+    public static void main(){
+         int a =1;
+         int i = 0;
+         System.out.print(a+",");
+         for(i=0;i<8;i++){
+              a = (a*10) + 1;
+              System.out.print(a+",");
+         }
+    }
+}
