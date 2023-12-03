7f0c48cf8171cb4d1ebe1b66e5fd696882f5b267
Arnav Bhagwat <76836227+CodexCS@users.noreply.github.com>
2023-12-03 23:29:48 +0530

    0,3,8,15...

---
+++ series4.java
@@ -0,0 +1,25 @@
+
+/**
+ * Write a description of class series4 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class series4
+{
+    public static void main(){
+        int a= 0;
+        int k = 3;
+        int i = 0;
+        System.out.print(a+" ");
+        for(i=0;i<10;i++){
+             a = a + k;
+             k = k + 2;
+             
+             System.out.print(a+" ");
+            
+            
+            
+        }
+    }
+}
