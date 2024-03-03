8f4b8791570839c9aeea921daf40fcd589c90405
Arnav Bhagwat <76836227+CodexCS@users.noreply.github.com>
2024-03-03 21:26:28 +0530

    Add files via upload

---
+++ HowManyPrimeNumberbtw1to100.java
@@ -0,0 +1,43 @@
+    
+/**
+ * Write a description of class HowManyPrimeNumberbtw1to100 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class HowManyPrimeNumberbtw1to100
+{
+    public static void main(){
+        int i = 0;
+        int m = 1;
+        int sum = 0;
+        int g = 2;
+        int c = 0;
+        int d = 0;
+        for( g = 2; g<=100;g++){
+                    sum = 0;
+                 for(i=1;i<=g;i++)
+                {
+                if(g%i==0)
+                {
+                  //System.out.println(i + " ");  
+                  sum = sum + i;
+                }
+            
+            }
+        
+            if(sum==(g+1)){
+                //System.out.println(g);
+                d++;
+                
+            }
+            
+                
+                
+            
+            }
+           System.out.print(d);
+            
+    } 
+    
+}
