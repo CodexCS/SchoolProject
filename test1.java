b980e0be4efb30aaa0c747618f847f3264b70b24
Arnav Bhagwat <76836227+CodexCS@users.noreply.github.com>
2024-03-04 18:46:38 +0530

    Add files via upload

---
+++ test1.java
@@ -0,0 +1,19 @@
+
+/**
+ * Write a description of class test1 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class test1
+{
+    public static void main(){
+        int m = 2;
+        int n = 15;
+        for(int i = 1;i<5;i++)
+        m++;
+        --n;
+        System.out.println("m="+ m);
+        System.out.println("n="+n);
+    }
+}
